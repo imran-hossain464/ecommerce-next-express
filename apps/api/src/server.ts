fd76438fcabe9b554c2eae6eea775988a6f3707a
Md. Imran Hossain <k.imran.khan464@gmail.com>
2026-06-20 15:40:18 +0600

    Update Sever.ts

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -14,7 +14,7 @@ const app = express();
 const port = Number(process.env.PORT ?? 4000);
 
 app.use(helmet());
-app.use(cors({ origin: process.env.WEB_ORIGIN ?? "http://localhost:3000" }));
+app.use(cors({ origin: process.env.WEB_ORIGIN ?? "https://ecommerce-next-express-qxm24p6ds.vercel.app", "http://localhost:3000" }));
 app.use(express.json({ limit: "10mb" }));
 
 app.get("/health", (_req, res) => {
